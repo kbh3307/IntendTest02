5b9788746c1c81004e60596369c35ad63a4e0b5e
kbh3307 <61855314+kbh3307@users.noreply.github.com>
2020-03-18 15:39:46 +0900

    메인 액티비티에서 목록/어댑터 생성 + 리스트뷰와 연결

--- app/src/main/java/kr/co/tjoeun/intendtest02/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/intendtest02/MainActivity.java
@@ -5,9 +5,17 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.tjoeun.intendtest02.adapters.RoomAdapter;
 import kr.co.tjoeun.intendtest02.databinding.ActivityMainBinding;
+import kr.co.tjoeun.intendtest02.datas.Room;
 
 public class MainActivity extends BaseActivity {
+
+    List<Room> roomDatas = new ArrayList<>();
+    RoomAdapter roomAdapter = null;
 
     ActivityMainBinding binding = null;
 
@@ -26,6 +34,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
-
+        roomAdapter = new RoomAdapter(mContext, R.layout.room_list_item, roomDatas);
+        binding.roomListView.setAdapter(roomAdapter);
     }
 }
