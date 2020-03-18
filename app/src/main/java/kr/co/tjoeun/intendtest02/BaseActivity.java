49ab557045d00746f792fc05a04d117be405e9f9
kbh3307 <61855314+kbh3307@users.noreply.github.com>
2020-03-18 14:44:28 +0900

    프로젝트 기본 구성 완료

---
+++ app/src/main/java/kr/co/tjoeun/intendtest02/BaseActivity.java
@@ -0,0 +1,13 @@
+package kr.co.tjoeun.intendtest02;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+}
