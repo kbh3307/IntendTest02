c8770fb5a22fc1be5fc5ce20e1ccd15b1135aedb
kbh3307 <61855314+kbh3307@users.noreply.github.com>
2020-03-18 15:46:58 +0900

    방 데이터 추가 => roomDatas에 대입

--- app/src/main/java/kr/co/tjoeun/intendtest02/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/intendtest02/MainActivity.java
@@ -36,5 +36,18 @@ public class MainActivity extends BaseActivity {
     public void setValues() {
         roomAdapter = new RoomAdapter(mContext, R.layout.room_list_item, roomDatas);
         binding.roomListView.setAdapter(roomAdapter);
+
+        addRoom();
     }
+
+    private void addRoom() {
+        roomDatas.add(new Room(8000,"서울시 은평구", 4, "살기 좋은 방입니다."));
+        roomDatas.add(new Room(7000,"서울시 노원구", 2, "서울치고 쌉니다."));
+        roomDatas.add(new Room(3000,"인천시 부평구", -1, "사지마세요."));
+        roomDatas.add(new Room(5000,"경기도 고양시", 0, "고양이 많아요."));
+        roomDatas.add(new Room(18000,"경기도 부천시", 5, "가격이 많이 비쌉니다."));
+
+        roomAdapter.notifyDataSetChanged();
+    }
+
 }
