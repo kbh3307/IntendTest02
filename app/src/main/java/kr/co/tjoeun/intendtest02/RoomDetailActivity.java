c053e97dd4ff56e6548f8746b2c1cbabb59d2e44
kbh3307 <61855314+kbh3307@users.noreply.github.com>
2020-03-18 17:13:37 +0900

    방 데이터 전달 1) Room 클래스 : implements Serializable 2) intent.putExtra("room", 방변수통째로) 3) 받아줄 때 (Room) getSerializableExtra

--- app/src/main/java/kr/co/tjoeun/intendtest02/RoomDetailActivity.java
+++ app/src/main/java/kr/co/tjoeun/intendtest02/RoomDetailActivity.java
@@ -6,6 +6,7 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 import kr.co.tjoeun.intendtest02.databinding.ActivityRoomDetailBinding;
+import kr.co.tjoeun.intendtest02.datas.Room;
 
 public class RoomDetailActivity extends BaseActivity {
 
@@ -17,5 +18,18 @@ public class RoomDetailActivity extends BaseActivity {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_room_detail);
         setupEvents();
         setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+//        첨부된 방 데이터를 받아서 화면에 출력
+        Room room = (Room) getIntent().getSerializableExtra("room");
+
     }
 }
